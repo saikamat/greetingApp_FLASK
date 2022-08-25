efa4ef93243596fef692d3a65dd6b8a0c4f0def4
saikamat <sai.s.kamat@gmail.com>
2022-08-25 20:39:44 +0200

    adding params

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import requests
 
 app = Flask(__name__)
 app.secret_key = "manbearpig_MUDMAN888"
-API_KEY='9f6686031db64b948313dd194bb4319b76cd5641c6cb6080d16ae494ce063678'
+API_KEY='b8d9dc5526a4bf3a2a0bd04a53e71565289f2bd562cc716e55d66a9ab5a6c38b'
 API_KEY_BASE64='dmVub204OTE0QG1haWwuY29tOjlmNjY4NjAzMWRiNjRiOTQ4MzEzZGQxOTRiYjQzMTliNzZjZDU2NDFjNmNiNjA4MGQxNmFlNDk0Y2UwNjM2Nzg='
 
 @app.route("/hello")
@@ -18,19 +18,28 @@ def greeter():
 	json_data = json.loads(request.data)
 	if json_data['message']['ticket_id']:
 		ticket_id = json_data['message']['ticket_id']
+		user = json_data['ticket']['assignee_user']['email']
+		email_text = 'Hi, our agent will be with you shortly.'
+		sender_id = json_data['message']['sender']['id']
+		receiver_id= json_data['message']['receiver']['id']
+		receiver_email= json_data['message']['receiver']['email']
+		sender_name = json_data['message']['sender']['firstname']
+		receiver_name = json_data['message']['receiver']['firstname']
+		# password = API_KEY_BASE64
 		print(ticket_id)
 		print('send email response')
-		send_email_response(ticket_id)
+		# ideally I'd have preferred passing as a JSON object.
+		send_email_response(ticket_id, user, email_text, sender_id, receiver_id, receiver_email, sender_name, receiver_name)
 
-		print('send internal email')
+		#print('send internal email')
 		# send_internal_mail(ticket_id)
 		return 'success', 200
 		# return render_template("index.html")
 
 
-def send_email_response(ticket_id):
+def send_email_response(ticket_id, user, email_text, sender_id, receiver_id, receiver_email, sender_name, receiver_name):
 	# print(ticket_id)
-	response_url = "https://saikamat2.gorgias.com/api/tickets/"+str(ticket_id)+"/messages"
+	response_url = "https://saikamat3.gorgias.com/api/tickets/"+str(ticket_id)+"/messages"
 	print(response_url)
 	payload = json.dumps({
 		"channel": "email",
@@ -38,33 +47,33 @@ def send_email_response(ticket_id):
 		"source": {
 			"type": "email",
 			"from": {
-			"id": 11942663,
-			"name": "Saikamat2 Support",
-			"address": "venom8914@mail.com"
+			"id": sender_id,
+			"name": sender_name,
+			"address": user
 			},
 			"to": [
 			{
-				"id": 11944965,
-				"name": "Sai",
-				"address": "sai.s.kamat@gmail.com"
+				"id": receiver_id,
+				"name": receiver_name,
+				"address": receiver_email
 			}
 			]
 		},
 		"via": "helpdesk",
-		"body_html": "Hello,<br><br>\n\n        I can't place an order on your site, it says: I don't have enough credit.<br>\n        How can I add some credits?<br><br>\n\n        Cheers,<br>\n        John Doe\n        ",
-		"body_text": "Hello,\n\n        I can't place an order on your site, it says: I don't have enough credit.\n        How can I add some credits?\n\n        Cheers,\n        John Doe\n        ",
+		"body_html": email_text,
+		"body_text": email_text,
 		"created_datetime": "2022-08-25T19:28:21.468912",
 		"external_id": "",
 		"failed_datetime": None,
 		"message_id": "<123345676453.2445.234@web>",
 		"receiver": {
-			"id": 11944965
+			"id": receiver_id
 		},
 		"sender": {
-			"id": 11942663
+			"id": sender_id
 		},
 		"sent_datetime": "2022-08-25T19:28:21.468912",
-		"subject": "Re:Refund request"
+		"subject": "Thank you for reaching out"
 	})
 	headers = {
 		"Accept": "application/json",
@@ -80,7 +89,7 @@ def send_email_response(ticket_id):
 
 
 def send_internal_mail(ticket_id):
-	response_url = "https://saikamat2.gorgias.com/api/tickets/"+str(ticket_id)+"/messages"
+	response_url = "https://saikamat3.gorgias.com/api/tickets/"+str(ticket_id)+"/messages"
 	print('in internal emailL:****' + response_url)
 	payload = json.dumps({
 		"channel": "email",
