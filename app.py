c683d5325ac552e90c3b5fbfee146cd0accc0127
saikamat <sai.s.kamat@gmail.com>
2022-08-25 13:58:35 +0200

    request data formatting

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ def index():
 def greeter():
 	# data = request.form['ticket_id']
 	print("\nrequest.data={}".format(request.data))
+	print(json.dumps(request.data['comment']))
 	return request.data
 	# flash("Hi " +json.dumps(request.json)+ ", great to see you!")
 	# if data:
