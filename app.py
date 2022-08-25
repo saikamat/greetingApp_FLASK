6bfebca420396a22f1d14bec381c612d5f59a167
saikamat <sai.s.kamat@gmail.com>
2022-08-25 21:07:58 +0200

    remove internal note func

--- app.py
+++ app.py
@@ -2,9 +2,10 @@ from urllib.request import urlopen
 from flask import Flask, render_template, request, flash
 import json
 import requests
+from datetime import datetime
 
 app = Flask(__name__)
-app.secret_key = "manbearpig_MUDMAN888"
+# app.secret_key = "manbearpig_MUDMAN888"
 API_KEY='2d7b9cdbd447b9209792375990f6f7d6fce1b2cbc39f5aaa7406bf7669520d1e'
 API_KEY_BASE64='a2Fpc2hhbWF0ODkrMDJAZ21haWwuY29tOjJkN2I5Y2RiZDQ0N2I5MjA5NzkyMzc1OTkwZjZmN2Q2ZmNlMWIyY2JjMzlmNWFhYTc0MDZiZjc2Njk1MjBkMWU='
 
@@ -20,6 +21,7 @@ def greeter():
 		ticket_id = json_data['message']['ticket_id']
 		user = json_data['ticket']['assignee_user']['email']
 		email_text = 'Hi, our agent will be with you shortly.'
+		message_text = 'Priority #1'
 		sender_id = json_data['message']['sender']['id']
 		receiver_id= json_data['message']['receiver']['id']
 		receiver_email= json_data['message']['receiver']['email']
@@ -32,7 +34,7 @@ def greeter():
 		send_email_response(ticket_id, user, email_text, sender_id, receiver_id, receiver_email, sender_name)
 
 		#print('send internal email')
-		send_internal_mail(ticket_id, user, email_text, sender_id)
+		# send_internal_mail(ticket_id, user, message_text, sender_id)
 		return 'success', 200
 		# return render_template("index.html")
 
@@ -61,7 +63,7 @@ def send_email_response(ticket_id, user, email_text, sender_id, receiver_id, rec
 		"via": "helpdesk",
 		"body_html": email_text,
 		"body_text": email_text,
-		"created_datetime": "2022-08-25T19:28:21.468912",
+		"created_datetime": datetime.now(),
 		"external_id": "",
 		"failed_datetime": None,
 		"message_id": "<123345676453.2445.234@web>",
@@ -71,7 +73,7 @@ def send_email_response(ticket_id, user, email_text, sender_id, receiver_id, rec
 		"sender": {
 			"id": sender_id
 		},
-		"sent_datetime": "2022-08-25T19:28:21.468912",
+		"sent_datetime": datetime.now(),
 		"subject": "Thank you for reaching out"
 	})
 	headers = {
@@ -87,7 +89,7 @@ def send_email_response(ticket_id, user, email_text, sender_id, receiver_id, rec
 	print(response.text)
 
 
-def send_internal_mail(ticket_id, user, email_text, sender_id):
+def send_internal_mail(ticket_id, user, message_text, sender_id):
 	response_url = "https://sephora.gorgias.com/api/tickets/"+str(ticket_id)+"/messages"
 	print(response_url)
 	payload = json.dumps({
@@ -109,9 +111,9 @@ def send_internal_mail(ticket_id, user, email_text, sender_id):
 			]
 		},
 		"via": "helpdesk",
-		"body_html": email_text,
-		"body_text": email_text,
-		"created_datetime": "2022-08-25T19:28:21.468912",
+		"body_html": message_text,
+		"body_text": message_text,
+		"created_datetime": datetime.now(),
 		"external_id": "",
 		"failed_datetime": None,
 		"message_id": "<123345676453.2445.234@web>",
@@ -121,7 +123,7 @@ def send_internal_mail(ticket_id, user, email_text, sender_id):
 		"sender": {
 			"id": sender_id
 		},
-		"sent_datetime": "2022-08-25T19:28:21.468912",
+		"sent_datetime": datetime.now(),
 		"subject": "Thank you for reaching out"
 	})
 	headers = {
